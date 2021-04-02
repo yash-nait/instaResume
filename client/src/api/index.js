d362158f52d4254d3c955e8e692c44fa1f26fc3f
yash-nait <yashnait@gmail.com>
2021-04-02 12:18:20 +0530

    fix error api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api'
+    baseURL: 'https://insta-resume.herokuapp.com//api'
 });
 
 export const insertResume = payload => api.post("/resume", payload);
